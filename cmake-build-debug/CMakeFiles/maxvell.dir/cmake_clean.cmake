709309ee7beba3f98d854756bcb5a0b1c7e2a2ad
Kiseliov <dmitrykiseliov2000@gmail.com>
2018-04-08 15:14:43 +0300

    ?

---
+++ cmake-build-debug/CMakeFiles/maxvell.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maxvell.dir/main.cpp.obj"
+  "maxvell.pdb"
+  "maxvell.exe"
+  "maxvell.exe.manifest"
+  "libmaxvell.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/maxvell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
